b921339ecbf431aa591e502512777d903135378d
ZimovOleg <zimov.oleg@gmail.com>
2022-07-20 14:56:00 +0400

    added

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldTestPost() {
+        given()
+            .baseUri("https://postman-echo.com")
+            .body("Test test")
+        .when()
+            .post("/post")
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("Test test"))
+        ;
+    }
+}
